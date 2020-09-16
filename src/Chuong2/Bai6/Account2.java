f0c328808e84ca14844993c7eb712016cefb4463
nbnguyen1997 <hoctapbinhnguyen@gmail.com>
2020-09-17 01:52:58 +0700

    Sử Hà Thuỳ Duyên

---
+++ src/Chuong2/Bai6/Account2.java
@@ -0,0 +1,69 @@
+package Chuong2.Bai6;
+
+public class Account2 {
+    long balance;
+    boolean ban = false;
+
+    Account2() {
+        balance = 0;
+    }
+
+    public synchronized void setBalance(long amount) {
+        if (ban) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        balance = amount;
+        ban = true;
+        notify();
+    }
+
+    public synchronized long getBalance() {
+        if (!ban) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        ban = false;
+        notify();
+        return balance;
+    }
+}
+
+class XuLy implements Runnable {
+    Account2 acc;
+    long amount;
+    String name;
+    Thread t;
+
+    public XuLy(Account2 acc, long amount, String name) {
+        this.acc = acc;
+        this.amount = amount;
+        this.name = name;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        acc.setBalance(amount);
+        System.out.println("So tien cua tk " + name + " la: " + acc.getBalance());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Account2 acc = new Account2();
+        XuLy xl1 = new XuLy(acc, 15, "Nguyen");
+        XuLy xl2 = new XuLy(acc, 30, "Duyen");
+        XuLy xl3 = new XuLy(acc, 45, "Anh");
+    }
+}
---
+++ src/Chuong2/bai4/SoNguyenTo.java
@@ -0,0 +1,12 @@
+package Chuong2.bai4;
+
+public class SoNguyenTo {
+    public static void main(String[] args) {
+
+    }
+}
+class TimSoNguyenTo{
+    int a;
+    int b;
+
+}
