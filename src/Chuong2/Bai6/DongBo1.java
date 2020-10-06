c298512da74ac738e8a7785605da5862bb08330d
nbnguyen1997 <hoctapbinhnguyen@gmail.com>
2020-10-07 00:14:46 +0700

    Các ví dụ chương 2

--- src/Chuong2/Bai6/DongBo1.java
+++ src/Chuong2/Bai6/DongBo1.java
@@ -1,41 +1,43 @@
 package Chuong2.Bai6;
 
-public class DongBo1 {
-    public static void main(String[] args) {
-Caller cl = new Caller("Nguyen");
-Thread t1 = new Thread(new CallMe(cl,1000));
-t1.start();
-Thread t2 = new Thread(new CallMe(cl,500));
-t2.start();
-    }
-}
-class CallMe implements Runnable{
-Caller cl ;
-int delay;
-
-    public CallMe(Caller cl, int delay) {
-        this.cl = cl;
-        this.delay = delay;
-    }
-
-    @Override
-    public void run() {
-synchronized (cl){
-    for (int i = 0; i < 5; i++) {
-        System.out.println(cl.name +" " + delay);
-        try{
-            Thread.sleep(delay);
+class Callme {
+    synchronized void call (String msg,int num){
+//    void call (String msg,int num){
+        System.out.println("["+num+msg);
+        try {
+            Thread.sleep(3500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println(num+"]");
     }
 }
+class Caller implements Runnable{
+    String msg;
+    Callme target;
+    Thread t;
+    int num;
+    public Caller(Callme tar, String s, int num){
+        target=tar;
+        msg=s;
+        t= new Thread(this);
+        t.start();
+        this.num=num;
+    }
+    @Override
+    public void run() {
+        target.call(msg,num);
     }
 }
-class Caller{
-    String name;
-
-    public Caller(String name) {
-        this.name = name;
+public class DongBo1 {
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Callme target1 = new Callme();
+        Caller ob1 = new Caller(target,"Nguyen",1);
+        Caller ob2 = new Caller(target,"Duyen",1);
+        Caller ob4 = new Caller(target1,"Test1",2);
+        Caller ob5 = new Caller(target1,"train1",2);
+        Caller ob6 = new Caller(target1,"I'm IT ",2);
+        Caller ob3 = new Caller(target,"Hello",1);
     }
 }
